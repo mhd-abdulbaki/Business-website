6c39f8637b58565a5e5b2e3a53aeaa8c5690ced2
Mhd <homamsy64@gmail.com>
2022-01-10 22:57:10 +0200

    Business website full responsive design, using HTML, modern CSS techniques such as CSS Grid, Flexbox, media query and Javascript

---
+++ JS/main.js
@@ -0,0 +1,87 @@
+// up button
+let upBtn = document.getElementById("up");
+
+window.addEventListener("scroll",function () {
+  let AscrollY =window.scrollY;
+  if (AscrollY >= 900) {
+    upBtn.style.display = "block";
+  } else {
+    upBtn.style.display = "none";
+  }
+})
+upBtn.onclick = () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// Countdown
+
+let countDown = new Date("Sep , 2022 21:59:59").getTime();
+
+let counter = setInterval(() => {
+  let dateNow = new Date().getTime();
+
+  let dateDiff = countDown - dateNow;
+
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
+
+// State Section
+
+let stateSection = document.getElementById("state");
+let numbers = document.querySelectorAll(".box .number");
+let started = false;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= stateSection.offsetTop - 200) {
+    if (!started) {
+      numbers.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+})
+
+function startCount(ele) {
+  let goal = ele.dataset.goal;
+  let count = setInterval(() => {
+    ele.textContent++;
+    if (ele.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 5);
+}
+
+// Skills
+
+let ourSkills = document.getElementById("our-skills");
+let progresses = document.querySelectorAll(".progress span");
+
+window.addEventListener('scroll',function () {
+
+  if (window.scrollY >= ourSkills.offsetTop - 200) {
+    progresses.forEach((prog) => {
+      prog.style.width = prog.dataset.width;
+    });
+  }
+})
+
+// burger-menu
+
+let burgerMenu = document.getElementById("burger-menu");
+let mainNav = document.getElementById("main-nav");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  mainNav.classList.toggle("active");
+});
