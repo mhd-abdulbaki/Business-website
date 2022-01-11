e07b73a1e9406e2e6e0b697cc7d058cff7a6331a
Mhd <homamsy64@gmail.com>
2022-01-11 03:25:54 +0200

    update

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,25 @@
+// Typing Effect
+
+const texts = ["To 3BDULBAKI"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 350);
+})();
+
 // up button
 let upBtn = document.getElementById("up");
 
